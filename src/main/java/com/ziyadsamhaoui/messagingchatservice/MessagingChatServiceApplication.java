a8608d9ebbf3af9e92f910a6a99279870bbdcc75
Ziyad SAMHAOUI <ziyad.samhaoui@usms.ac.ma>
2026-09-17 13:11:21 +0100

    initial commit

---
+++ src/main/java/com/ziyadsamhaoui/messagingchatservice/MessagingChatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ziyadsamhaoui.messagingchatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingChatServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingChatServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ziyadsamhaoui/messagingchatservice/MessagingChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziyadsamhaoui.messagingchatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingChatServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
